0aee7ff1539f79340921a5dabcfea23a6812db70
YuXin1314520 <97359012@qq.com>
2025-06-03 14:01:46 +0800

    Update DYYYSettingViewController.m

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -549,8 +549,8 @@ extern CFStringRef kUTTypeMovie;
     
     CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
-    CGFloat containerWidth = MIN(screenWidth - 80, 320); 
-    CGFloat containerHeight = MIN(screenHeight - 80, 450);
+    CGFloat containerWidth = MIN(screenWidth - 80, 480); 
+    CGFloat containerHeight = MIN(screenHeight - 80, 320);
     
     self.containerBlurView.frame = CGRectMake(0, 0, containerWidth, containerHeight);
     self.containerBlurView.center = self.view.center;
