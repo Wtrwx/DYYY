56d111183a95140e2738c1a92105b357156506c6
维他入我心 <lujialin@qhu.edu.cn>
2025-06-26 13:03:22 +0800

    Fix missing import for Geo DB picker

--- DYYYSettingsHelper.m
+++ DYYYSettingsHelper.m
@@ -3,6 +3,7 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 #import "DYYYImagePickerDelegate.h"
+#import "DYYYBackupPickerDelegate.h"
 
 #import "DYYYAboutDialogView.h"
 #import "DYYYCustomInputView.h"
